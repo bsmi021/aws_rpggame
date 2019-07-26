79160185885955aea2bceebe34e048c3ed139035
bsmi021@gmail.com <bsmi021@gmail.com>
2019-07-26 03:55:52 -0400

    removed config

--- assets/src/config.js
+++
@@ -1,7 +0,0 @@
-export default {
-    apiGateway: {
-        REGION: "us-east-2",
-        API_URL: "",
-        ITEMS_API_URL: "https://krjgrmiq4f.execute-api.us-east-2.amazonaws.com/dev"
-    }
-}
